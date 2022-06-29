a58bda5a28525a10d35872edb3930df084ed8225
João Benthin <joaopaulobenthin@hotmail.com>
2022-06-29 14:55:24 -0300

    ReactJS fundamentals

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Post } from './Post';
+
+function App() {
+  return (
+    <>
+      <Post
+        author="Joao Benthin"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dignissimos aut vero, reiciendis optio dolorum quam maxime similique, explicabo officiis nam necessitatibus magnam. Nisi dolore repellat recusandae sequi eveniet mollitia."
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
+    </>
+  )
+}
+
+export default App
+
